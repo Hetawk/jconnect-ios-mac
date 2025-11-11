568e8aaea5ee31bd8b6f689fc5357b1b8968fe37
Enoch Kwateh Dongbo <enoch.dongbo@gmail.com>
2025-11-11 18:46:48 +0800

    Initial Commit

---
+++ jicfconnect/jicfconnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  jicfconnectApp.swift
+//  jicfconnect
+//
+//  Created by Enoch Kwateh Dongbo on 2025/11/11.
+//
+
+import SwiftUI
+
+@main
+struct jicfconnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ jicfconnectTests/jicfconnectTests.swift
@@ -0,0 +1,16 @@
+//
+//  jicfconnectTests.swift
+//  jicfconnectTests
+//
+//  Created by Enoch Kwateh Dongbo on 2025/11/11.
+//
+
+import Testing
+
+struct jicfconnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
